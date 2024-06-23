11fa30bd988092e4ce804288821204e62988aa27
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-06-23 12:07:07 +0530

    "Refactor main.py to use threading and asyncio.gather"

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bot import Bot
 from zenova import zenova_boot, zenova_bot
 import asyncio
+import threading
 
-loop = asyncio.get_event_loop()
-
-
-loop.run_until_complete(zenova_bot())
-Bot().run()
-loop.run_until_complete(zenova_boot())
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    bot_thread = threading.Thread(target=Bot().run)
+    bot_thread.start()
+    loop.run_until_complete(asyncio.gather(zenova_bot(), zenova_boot()))
