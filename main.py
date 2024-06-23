cc335e9ba61fb41bc9823106516fd72f0d1b8ace
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-06-23 12:15:36 +0530

    "Removed threading and imported asyncio at top; refactored Bot().run to async function and added to asyncio.gather"

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import asyncio
 from bot import Bot
 from zenova import zenova_boot, zenova_bot
-import asyncio
-import threading
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    bot_thread = threading.Thread(target=Bot().run)
-    bot_thread.start()
-    loop.run_until_complete(asyncio.gather(zenova_bot(), zenova_boot()))
+
+    async def run_bot():
+        bot = Bot()
+        await bot.run()
+
+    loop.run_until_complete(asyncio.gather(zenova_bot(), zenova_boot(), run_bot()))
