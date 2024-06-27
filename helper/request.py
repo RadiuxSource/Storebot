6ac15122b5df1f29868bb20654a2bfa26b9e9e75
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-06-27 10:02:42 +0530

    remove lru_Cac

--- helper/request.py
+++ helper/request.py
@@ -1,8 +1,8 @@
 import requests
 import asyncio
-from functools import lru_cache
+# from functools import lru_cache
 
-@lru_cache(maxsize=None) 
+# @lru_cache(maxsize=None) 
 async def get_teachers(subject):
     response = requests.get(f"https://zenova-lec-api.vercel.app/teachers?subject={subject}")
     if response.status_code == 200:
@@ -10,7 +10,7 @@ async def get_teachers(subject):
     else:
         return None
 
-@lru_cache(maxsize=None)  
+# @lru_cache(maxsize=None)  
 async def get_chapters(subject, teacher_name):
     response = requests.get(f"https://zenova-lec-api.vercel.app/chapters?subject={subject}&teacher={teacher_name}")
     if response.status_code == 200:
