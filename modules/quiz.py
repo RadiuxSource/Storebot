31cc1fc17555a1392bc1cbabf83cff1a8f83f93a
store-x <anipgupta31@gmail.com>
2024-10-25 19:53:05 +0530

    ok

--- modules/quiz.py
+++ modules/quiz.py
@@ -49,100 +49,100 @@ Name = {member.chat.title}
             pass
 
 
-@zenova.on_message(filters.command('quiz') & filters.group)
-async def quiz_mode(client, message: Message):
-    chat_id = message.chat.id
-    present, count = await present_group(chat_id)
-    if not present:
-        try:
-            await add_group(chat_id)
-            INFO = f'''
-#NewChat
+# @zenova.on_message(filters.command('quiz') & filters.group)
+# async def quiz_mode(client, message: Message):
+#     chat_id = message.chat.id
+#     present, count = await present_group(chat_id)
+#     if not present:
+#         try:
+#             await add_group(chat_id)
+#             INFO = f'''
+# #NewChat
 
-Total chats = [{int(count) + 1}]
-Chat id = {chat_id}
-Name = {message.chat.title}
-'''
-            await zenova.send_message(LOGGER_ID, INFO)
-        except:
-            pass
-    msg_id = message.id
-    # # if group type is not group
-    # if message.chat.type != 'GROUP' or message.chat.type != 'SUPERGROUP':
-    #     await message.reply('🚫 This command can only be used in groups.', reply_to_message_id=msg_id)
-    #     return
-    if not await is_admin(message.chat.id, message.from_user.id):
-        await message.reply('🚫 You don\'t have access to this command.', reply_to_message_id=msg_id)
-        return
-    args = message.text.split()
-    if len(args) < 2:
-        await message.reply('🚫 Wrong usage. Use /quiz on/off', reply_to_message_id=msg_id)
-        return
-    mode = args[1]
-    if mode not in ['on', 'off']:
-        await message.reply('🚫 Wrong usage. Use /quiz on/off', reply_to_message_id=msg_id)
-        return
-    unpresent = await unpresent_quizzez(chat_id)
-    if mode == 'on':
-        if not unpresent:
-            await message.reply('🚫 Quiz mode is already on in this chat.', reply_to_message_id=msg_id)
-            return
-        await del_quizzez(chat_id)
-        await message.reply('🟢 Quiz mode is now on in this chat.', reply_to_message_id=msg_id)
-    else:
-        if unpresent:
-            await message.reply('🚫 Quiz mode is already off in this chat.', reply_to_message_id=msg_id)
-            return
-        await add_quizzez(chat_id)
-        await message.reply('🔴 Quiz mode is now off in this chat.', reply_to_message_id=msg_id)
+# Total chats = [{int(count) + 1}]
+# Chat id = {chat_id}
+# Name = {message.chat.title}
+# '''
+#             await zenova.send_message(LOGGER_ID, INFO)
+#         except:
+#             pass
+#     msg_id = message.id
+#     # # if group type is not group
+#     # if message.chat.type != 'GROUP' or message.chat.type != 'SUPERGROUP':
+#     #     await message.reply('🚫 This command can only be used in groups.', reply_to_message_id=msg_id)
+#     #     return
+#     if not await is_admin(message.chat.id, message.from_user.id):
+#         await message.reply('🚫 You don\'t have access to this command.', reply_to_message_id=msg_id)
+#         return
+#     args = message.text.split()
+#     if len(args) < 2:
+#         await message.reply('🚫 Wrong usage. Use /quiz on/off', reply_to_message_id=msg_id)
+#         return
+#     mode = args[1]
+#     if mode not in ['on', 'off']:
+#         await message.reply('🚫 Wrong usage. Use /quiz on/off', reply_to_message_id=msg_id)
+#         return
+#     unpresent = await unpresent_quizzez(chat_id)
+#     if mode == 'on':
+#         if not unpresent:
+#             await message.reply('🚫 Quiz mode is already on in this chat.', reply_to_message_id=msg_id)
+#             return
+#         await del_quizzez(chat_id)
+#         await message.reply('🟢 Quiz mode is now on in this chat.', reply_to_message_id=msg_id)
+#     else:
+#         if unpresent:
+#             await message.reply('🚫 Quiz mode is already off in this chat.', reply_to_message_id=msg_id)
+#             return
+#         await add_quizzez(chat_id)
+#         await message.reply('🔴 Quiz mode is now off in this chat.', reply_to_message_id=msg_id)
 
 
-async def get_question():
-    blackbox = Blackbox()
-    sys_prompt, subject = await quiz_ai()
-    content = f"Generate a question for the {subject} subject."
-    response = blackbox.request(content, sys_prompt)
-    # response = requests.get(f"{API_URL}{await quiz_ai()}")
-    result = response.json()
-    question = result.get("answer", "No answer received from ChatGPT.")
-    return question
+# async def get_question():
+#     blackbox = Blackbox()
+#     sys_prompt, subject = await quiz_ai()
+#     content = f"Generate a question for the {subject} subject."
+#     response = blackbox.request(content, sys_prompt)
+#     # response = requests.get(f"{API_URL}{await quiz_ai()}")
+#     result = response.json()
+#     question = result.get("answer", "No answer received from ChatGPT.")
+#     return question
 
-async def send_quiz():
-    groups = await full_groupbase()
-    quizzez_chats = await full_quizzezbase()
-    for chat_id in groups:
-        if chat_id not in quizzez_chats:
-            await send_polls(int(chat_id))
+# async def send_quiz():
+#     groups = await full_groupbase()
+#     quizzez_chats = await full_quizzezbase()
+#     for chat_id in groups:
+#         if chat_id not in quizzez_chats:
+#             await send_polls(int(chat_id))
             
-async def send_polls(chat_id: int):
-    try:
-        question = await get_question()
-        question = json.loads(question)
-        options = question["options"]
-        correct_option_id = question["correct_option_id"]
-        poll = await zenova.send_poll(int(chat_id), question["question"], options, is_anonymous= False, type=PollType.QUIZ, correct_option_id= int(correct_option_id), explanation= 'Note: AI-generated quiz. Verify answers independently.',)
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return await send_polls(chat_id)
-    except InputUserDeactivated:
-        print(f"{chat_id} : Deactivated")
-        failed_chats.append(chat_id)
-        return 400
-    except PeerIdInvalid:
-        print(f"{chat_id} : PeerIdInvalid")
-        failed_chats.append(chat_id)
-        return 400
-    except ChatWriteForbidden:
-        print(f"{chat_id} : Invalid ID")
-        failed_chats.append(chat_id)
-        return 400
-    except MessageTooLong:
-        print(f"{chat_id} : Message too long")
-        return await send_polls(chat_id)
-    except Exception as e:
-        print(f"Error sending quiz to chat {chat_id}: {e}")
-        return 500
+# async def send_polls(chat_id: int):
+#     try:
+#         question = await get_question()
+#         question = json.loads(question)
+#         options = question["options"]
+#         correct_option_id = question["correct_option_id"]
+#         poll = await zenova.send_poll(int(chat_id), question["question"], options, is_anonymous= False, type=PollType.QUIZ, correct_option_id= int(correct_option_id), explanation= 'Note: AI-generated quiz. Verify answers independently.',)
+#     except FloodWait as e:
+#         await asyncio.sleep(e.value)
+#         return await send_polls(chat_id)
+#     except InputUserDeactivated:
+#         print(f"{chat_id} : Deactivated")
+#         failed_chats.append(chat_id)
+#         return 400
+#     except PeerIdInvalid:
+#         print(f"{chat_id} : PeerIdInvalid")
+#         failed_chats.append(chat_id)
+#         return 400
+#     except ChatWriteForbidden:
+#         print(f"{chat_id} : Invalid ID")
+#         failed_chats.append(chat_id)
+#         return 400
+#     except MessageTooLong:
+#         print(f"{chat_id} : Message too long")
+#         return await send_polls(chat_id)
+#     except Exception as e:
+#         print(f"Error sending quiz to chat {chat_id}: {e}")
+#         return 500
             
 
-scheduler.add_job(send_quiz, 'interval', minutes=30)
-scheduler.start()
+# scheduler.add_job(send_quiz, 'interval', minutes=30)
+# scheduler.start()
